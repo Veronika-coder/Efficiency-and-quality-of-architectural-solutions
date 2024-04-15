49938940ff6aca8d978c3eb7da825bf7093280ab
Veronika NTU KHPI <55531111+Veronika-coder@users.noreply.github.com>
2024-04-15 18:34:01 +0200

    Create output_1.py

---
+++ lab4/output_1.py
@@ -0,0 +1,20 @@
+class Animal:
+    def speak(self):
+        pass
+
+    def move(self):
+        pass
+
+class Dog:
+    def __init__(self):
+        self.animal = Animal()
+
+    def speak(self):
+        self.animal.speak()
+
+    def move(self):
+        self.animal.move()
+
+dog = Dog()
+dog.speak()
+dog.move()
